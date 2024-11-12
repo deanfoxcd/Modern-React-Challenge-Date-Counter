380d98b96efae66638f6c30a13c57f47860a6a54
Dean Fox <deanfoxcd@gmail.com>
2024-11-11 21:55:34 -0500

    code adjusted for slider and text input

--- src/App.js
+++ src/App.js
@@ -28,6 +28,11 @@ const Counter = () => {
     setCount((c) => c - step);
   };
 
+  const handleReset = () => {
+    setCount(0);
+    setStep(1);
+  };
+
   const currDate = new Date();
 
   const daysToAdd = count;
@@ -40,5 +45,52 @@ const Counter = () => {
   const date = currDate.getDate();
   const year = currDate.getFullYear();
 
-  return <div></div>;
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+        margin: 'auto',
+      }}
+    >
+      <div>
+        <input
+          type="range"
+          min="0"
+          max="10"
+          value={step}
+          onChange={(e) => setStep(Number(e.target.value))}
+        />
+        <span>Step: {step}</span>
+      </div>
+      <div>
+        <button onClick={handlePrevCount}> - </button>
+        <span>
+          <input
+            type="text"
+            value={count}
+            onChange={(e) => setCount(Number(e.target.value))}
+          />
+        </span>
+        <button onClick={handleNextCount}> + </button>
+      </div>
+      <p>
+        {count === 0
+          ? 'Today is '
+          : count > 0
+          ? `${count} days from today is `
+          : `${Math.abs(count)} days ago was `}
+        {day} {month} {date} {year}
+        {/* {step * count} days from today is  */}
+      </p>
+
+      {step !== 1 || count !== 0 ? (
+        <button onClick={handleReset}>RESET</button>
+      ) : (
+        ''
+      )}
+    </div>
+  );
 };
